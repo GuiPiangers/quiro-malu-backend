f47f4d8d5a4ebc1246283742429279711769e0d5
Guilherme Piangers <eduardomartinspiangers@gmail.com>
2024-10-13 13:48:24 -0300

    Create progress data table

---
+++ src/repositories/server/database/createProgressTable.ts
@@ -0,0 +1,18 @@
+import { query } from "../mySqlConnection"
+export async function createProgressTable(){
+    const sql = `CREATE TABLE IF NOT EXISTS progress(
+        patientId VARCHAR(50),
+        userId VARCHAR(50),
+        service VARCHAR(50),
+        actualProblem TEXT,
+        procedures TEXT,
+        date DATE,
+        createAt DATETIME DEFAULT CURRENT_TIMESTAMP,
+        updateAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+        FOREIGN KEY (userId) REFERENCES users(id) ON DELETE CASCADE ON UPDATE CASCADE
+        FOREIGN KEY (patientId) REFERENCES patients(id) ON DELETE CASCADE ON UPDATE CASCADE
+        )`
+    const errorMessage = `Não foi possível criar a tabela`
+    const result = await query(errorMessage, sql)
+    return result
+}
