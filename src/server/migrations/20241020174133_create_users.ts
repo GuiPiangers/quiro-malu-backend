5921d8fb6ca65a4eb0e5fc6082ba7bc13c6fd899
Guilherme Piangers <eduardomartinspiangers@gmail.com>
2024-10-20 15:01:07 -0300

    create users migration

--- src/server/migrations/20241020174133_create_users.ts
+++ src/server/migrations/20241020174133_create_users.ts
@@ -1,10 +1,15 @@
+import { table } from "console";
 import type { Knex } from "knex";
 
 
 export async function up(knex: Knex): Promise<void> {
+    return knex.schema.createTable('users', table => {
+        table.string('')
+    })
 }
 
 
 export async function down(knex: Knex): Promise<void> {
+    knex.schema.dropTable('users')
 }
 
