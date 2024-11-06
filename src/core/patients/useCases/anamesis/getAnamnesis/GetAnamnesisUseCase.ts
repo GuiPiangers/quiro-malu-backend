e38c22e9752fa922bb289b8b8c395acf54cdc19c
Guilherme <eduardomartinspiangers@gmail.com>
2024-11-06 20:22:50 +0000

    test: create setAnamnesisUsecase tests

--- src/core/patients/useCases/anamesis/getAnamnesis/GetAnamnesisUseCase.ts
+++ src/core/patients/useCases/anamesis/getAnamnesis/GetAnamnesisUseCase.ts
@@ -1,13 +1,14 @@
 import { IAnamnesisRepository } from "../../../../../repositories/anamnesis/IAnamnesisRepository";
 
 export class GetAnamnesisUseCase {
-    constructor(
-        private AnamnesisRepository: IAnamnesisRepository
-    ) { }
+  constructor(private AnamnesisRepository: IAnamnesisRepository) {}
 
-    async execute(patientId: string, userId: string) {
-        const [anamnesisData] = await this.AnamnesisRepository.get(patientId, userId)
+  async execute(patientId: string, userId: string) {
+    const [anamnesisData] = await this.AnamnesisRepository.get(
+      patientId,
+      userId,
+    );
 
-        return anamnesisData
-    }
+    return anamnesisData;
+  }
 }
