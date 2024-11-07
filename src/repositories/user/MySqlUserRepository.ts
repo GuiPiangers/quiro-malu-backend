63cdf60f8907e9123ade05310f3424bc6c3923c2
Guilherme <eduardomartinspiangers@gmail.com>
2024-11-07 16:18:28 +0000

    Change user repository to return only valid values when save or get d ata from DB

--- src/repositories/user/MySqlUserRepository.ts
+++ src/repositories/user/MySqlUserRepository.ts
@@ -1,25 +1,28 @@
 import { query } from "../../database/mySqlConnection";
 import { UserDTO } from "../../core/authentication/models/User";
 import { IUserRepository } from "./IUserRepository";
+import { getValidObjectValues } from "../../utils/getValidObjectValues";
 
 export class MySqlUserRepository implements IUserRepository {
   async getById(id: string): Promise<UserDTO[]> {
     const sql = "SELECT * FROM users WHERE id = ?";
     const errorMessage = `Não foi possível realizar a busca`;
+    const result = await query<UserDTO[]>(errorMessage, sql, id);
 
-    return await query(errorMessage, sql, id);
+    return result.map((user) => getValidObjectValues(user));
   }
 
   async getByEmail(email: string): Promise<UserDTO[]> {
     const sql = "SELECT * FROM users WHERE email = ?";
     const errorMessage = `Não foi possível realizar a busca`;
+    const result = await query<UserDTO[]>(errorMessage, sql, email);
 
-    return await query(errorMessage, sql, email);
+    return result.map((user) => getValidObjectValues(user));
   }
 
   async save(data: UserDTO): Promise<void> {
     const sql = "INSERT INTO users SET ?";
     const errorMessage = "Falha ao adicionar o usuário";
-    return await query(errorMessage, sql, data);
+    return await query(errorMessage, sql, getValidObjectValues(data));
   }
 }
