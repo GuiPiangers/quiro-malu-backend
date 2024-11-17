18180a9a03ad29bef6adc584e8896061baddeb17
Guilherme Piangers <eduardomartinspiangers@gmail.com>
2024-11-17 19:51:09 -0300

    refactor: extract to function validate if cpf not exist when update patient use case

--- src/core/patients/useCases/updatePatient/UpdatePatientUseCase.ts
+++ src/core/patients/useCases/updatePatient/UpdatePatientUseCase.ts
@@ -13,39 +13,35 @@ export class UpdatePatientUseCase {
     const patient = new Patient(data);
     const { location, id, ...patientDTO } = patient.getPatientDTO();
 
-    if (patient.cpf) {
-      const [verifyPatient] = await this.patientRepository.getByCpf(
-        patient.cpf,
-        userId,
-      );
-      if (verifyPatient.id && verifyPatient.id !== patient.id) {
-        throw new ApiError("Já existe um usuário cadastrado com esse CPF", 400);
-      }
-    }
+    await this.validateCpfNotExist({
+      cpf: patientDTO.cpf,
+      userId,
+      patientId: patient.id,
+    });
 
     const updatePatient = this.patientRepository.update(
       patientDTO,
-      id!,
+      patient.id,
       userId,
     );
 
     if (location) {
       const [validateLocation] = await this.locationRepository.getLocation(
-        id!,
+        patient.id,
         userId,
       );
 
       if (validateLocation) {
         const updateLocation = this.locationRepository.update(
           location,
-          id!,
+          patient.id,
           userId,
         );
         await Promise.all([updatePatient, updateLocation]);
       } else {
         const saveLocation = this.locationRepository.save(
           location,
-          id!,
+          patient.id,
           userId,
         );
         await Promise.all([updatePatient, saveLocation]);
@@ -55,5 +51,25 @@ export class UpdatePatientUseCase {
     }
 
     return patient;
+  }
+
+  private async validateCpfNotExist({
+    cpf,
+    patientId,
+    userId,
+  }: {
+    cpf?: string;
+    patientId: string;
+    userId: string;
+  }) {
+    if (cpf) {
+      const [verifyPatient] = await this.patientRepository.getByCpf(
+        cpf,
+        userId,
+      );
+      if (verifyPatient.id && verifyPatient.id !== patientId) {
+        throw new ApiError("Já existe um usuário cadastrado com esse CPF", 400);
+      }
+    }
   }
 }
