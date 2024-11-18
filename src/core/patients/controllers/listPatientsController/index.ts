7d0e20aba9419ad8049485f7486df6e2bcd503dd
Guilherme <eduardomartinspiangers@gmail.com>
2024-11-18 19:21:06 +0000

    refactor: change count all patients method to use Knex

--- src/core/patients/controllers/listPatientsController/index.ts
+++ src/core/patients/controllers/listPatientsController/index.ts
@@ -1,9 +1,9 @@
-import { MySqlPatientRepository } from "../../../../repositories/patient/MySqlPatientRepository";
+import { KnexPatientRepository } from "../../../../repositories/patient/KnexPatientRepository";
 import { ListPatientsUseCase } from "../../useCases/listPatients/ListPatientsUseCase";
 import { ListPatientsController } from "./ListPatientsController";
 
-const patientRepository = new MySqlPatientRepository()
-const listPatientsUseCase = new ListPatientsUseCase(patientRepository)
-const listPatientsController = new ListPatientsController(listPatientsUseCase)
+const patientRepository = new KnexPatientRepository();
+const listPatientsUseCase = new ListPatientsUseCase(patientRepository);
+const listPatientsController = new ListPatientsController(listPatientsUseCase);
 
-export { listPatientsController }
+export { listPatientsController };
