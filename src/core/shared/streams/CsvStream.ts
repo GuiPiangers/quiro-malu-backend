b9ce474788185d3ed72ffdc046efbc10215c96ee
Guilherme <eduardomartinspiangers@gmail.com>
2024-11-22 18:26:39 +0000

    feat: Improve CsvStream class

--- src/core/shared/streams/CsvStream.ts
+++ src/core/shared/streams/CsvStream.ts
@@ -1,20 +1,13 @@
-import { Readable, Transform } from "stream";
+import { Readable, Stream, Transform, Writable } from "stream";
 import { parse } from "fast-csv";
 
-export class CsvStream {
-  private stream: Readable;
-  constructor(buffer: Buffer) {
-    this.stream = new Readable({
-      read() {
-        this.push(buffer);
-        this.push(null);
-      },
-      objectMode: true,
-    }).pipe(parse({ headers: true }));
-  }
+abstract class IStream<chunk> {
+  constructor(protected stream: Stream) {}
 
-  transform(fn: (value: any) => any) {
-    this.stream = this.stream.pipe(
+  abstract transform<k = chunk>(fn: (value: chunk) => k): IStream<k>;
+
+  protected createTransform<k = chunk>(fn: (value: chunk) => k) {
+    return this.stream.pipe(
       new Transform({
         objectMode: true,
 
@@ -24,7 +17,46 @@ export class CsvStream {
         },
       }),
     );
+  }
 
-    return this;
+  write(fn: (value: chunk) => void) {
+    return this.stream.pipe(
+      new Writable({
+        objectMode: true,
+        write(chunk, end, cb) {
+          fn(chunk);
+          return cb();
+        },
+      }),
+    );
+  }
+}
+
+class TransformCsv<chunk> extends IStream<chunk> {
+  constructor(stream: Stream) {
+    super(stream);
+  }
+
+  transform<k = chunk>(fn: (value: chunk) => k): IStream<k> {
+    return new TransformCsv<k>(this.createTransform(fn));
+  }
+}
+
+export class CsvStream<chunk> extends IStream<chunk> {
+  constructor(buffer: Buffer) {
+    super(
+      new Readable({
+        read() {
+          this.push(buffer);
+          this.push(null);
+        },
+        objectMode: true,
+      }).pipe(parse({ headers: true })),
+    );
+  }
+
+  transform<k = chunk>(fn: (value: chunk) => k) {
+    const newStream = this.createTransform(fn);
+    return new TransformCsv<k>(newStream);
   }
 }
