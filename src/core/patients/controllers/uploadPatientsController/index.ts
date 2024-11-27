6c77def05c36b5ed8ca6908527f1a167bd67a5cf
Guilherme Piangers <eduardomartinspiangers@gmail.com>
2024-11-26 21:22:53 -0300

    feat: add location upload on uploadPatientsUseCase

--- src/core/patients/controllers/uploadPatientsController/index.ts
+++ src/core/patients/controllers/uploadPatientsController/index.ts
@@ -3,17 +3,26 @@ import { UploadPatientsController } from "./UploadPatientsController";
 import { KnexPatientRepository } from "../../../../repositories/patient/KnexPatientRepository";
 import { UploadPatientsUseCase } from "../../useCases/uploadPatients/UploadPatientsUseCase";
 import { Request, Response } from "express";
+import { MySqlAnamnesisRepository } from "../../../../repositories/anamnesis/MySqlAnamnesisRepository";
+import { MySqlDiagnosticRepository } from "../../../../repositories/diagnostic/MySqlDiagnosticRepository";
 
-const uplaodPatients = {
+const uploadPatients = {
   handle: (reques: Request, response: Response) => {
     const locationRepository = new KnexLocationRepository();
     const patientRepository = new KnexPatientRepository();
-    const uploadPatientsUseCase = new UploadPatientsUseCase(patientRepository);
-    const uplaodPatientsController = new UploadPatientsController(
+    const anamnesisRepository = new MySqlAnamnesisRepository();
+    const diagnosticRepository = new MySqlDiagnosticRepository();
+    const uploadPatientsUseCase = new UploadPatientsUseCase(
+      patientRepository,
+      locationRepository,
+      anamnesisRepository,
+      diagnosticRepository,
+    );
+    const uploadPatientsController = new UploadPatientsController(
       uploadPatientsUseCase,
     ).handle(reques, response);
-    return uplaodPatientsController;
+    return uploadPatientsController;
   },
 };
 
-export { uplaodPatients as uploadPatientsController };
+export { uploadPatients as uploadPatientsController };
