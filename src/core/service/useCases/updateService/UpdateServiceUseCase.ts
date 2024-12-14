39a0427fb3010be1467d61e88c20dd78a27e143b
Guilherme Piangers <eduardomartinspiangers@gmail.com>
2024-12-13 22:21:53 -0300

    pretty UpdateServiceUseCase

--- src/core/service/useCases/updateService/UpdateServiceUseCase.ts
+++ src/core/service/useCases/updateService/UpdateServiceUseCase.ts
@@ -3,18 +3,26 @@ import { IServiceRepository } from "../../../../repositories/service/IServiceRep
 import { ApiError } from "../../../../utils/ApiError";
 
 export class UpdateServiceUseCase {
-  constructor(private ServiceRepository: IServiceRepository) { }
+  constructor(private ServiceRepository: IServiceRepository) {}
 
   async execute({ userId, ...data }: ServiceDTO & { userId: string }) {
-    const service = new Service(data).getDTO()
+    const service = new Service(data).getDTO();
 
-    if (!data.id) throw new ApiError('O id deve ser informado', 400, 'service')
+    if (!data.id) throw new ApiError("O id deve ser informado", 400, "service");
 
-    const [verifyName] = await this.ServiceRepository.getByName({ name: service.name, userId })
-    if (verifyName && verifyName.id !== data.id) throw new ApiError('Já existe um serviço cadastrado com esse nome', 400, 'service')
+    const [verifyName] = await this.ServiceRepository.getByName({
+      name: service.name,
+      userId,
+    });
+    if (verifyName && verifyName.id !== data.id)
+      throw new ApiError(
+        "Já existe um serviço cadastrado com esse nome",
+        400,
+        "service",
+      );
 
     await this.ServiceRepository.update({ userId, ...service });
 
-    return service
+    return service;
   }
 }
