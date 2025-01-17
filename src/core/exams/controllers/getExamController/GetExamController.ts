151ca9844d96a7eab6f46578c55a2f4832a24c97
Guilherme Piangers <eduardomartinspiangers@gmail.com>
2025-01-17 20:59:08 -0300

    feat: create GetExamController and it instantiation

---
+++ src/core/exams/controllers/getExamController/GetExamController.ts
@@ -0,0 +1,36 @@
+import { Request, Response } from "express";
+import { ApiError } from "../../../../utils/ApiError";
+import { responseError } from "../../../../utils/ResponseError";
+import { GetExamUseCase } from "../../useCases/GetExamUSeCase";
+
+export class GetExamController {
+  constructor(private getExamUseCase: GetExamUseCase) {}
+
+  async handle(request: Request, response: Response) {
+    try {
+      const { id: userId } = request.user;
+      const { patientId, id } = request.params as {
+        patientId: string;
+        id: string;
+      };
+
+      const file = request.file;
+
+      if (!file) throw new ApiError("Nenhum arquivo enviado", 400);
+      if (!userId) throw new ApiError("Acesso não autorizado", 401);
+      if (!id) throw new ApiError("Nenhum paciente enviado", 400);
+
+      const url = await this.getExamUseCase.execute({
+        patientId,
+        userId,
+        id,
+      });
+
+      console.log(url);
+
+      response.status(200).json({ url });
+    } catch (error: any) {
+      responseError(response, error);
+    }
+  }
+}
