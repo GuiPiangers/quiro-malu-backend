247d340a36e26a147c45bbc3666b57284dbc4626
Guilherme <eduardomartinspiangers@gmail.com>
2025-01-17 12:50:20 +0000

    feat: create an Exam model class

---
+++ src/core/exams/models/Exam.ts
@@ -0,0 +1,34 @@
+import { Entity } from "../../shared/Entity";
+
+export type ExamDTO = {
+  id?: string;
+  patientId: string;
+  fileName: string;
+  fileType: string;
+  fileSize?: number;
+};
+
+export class Exam extends Entity {
+  readonly patientId: string;
+  readonly fileName: string;
+  readonly fileType: string;
+  readonly fileSize?: number;
+
+  constructor({ fileName, fileType, patientId, fileSize, id }: ExamDTO) {
+    super(id);
+    this.patientId = patientId;
+    this.fileName = fileName;
+    this.fileType = fileType;
+    this.fileSize = fileSize;
+  }
+
+  getDTO(): ExamDTO {
+    return {
+      id: this.id,
+      patientId: this.patientId,
+      fileName: this.fileName,
+      fileType: this.fileType,
+      fileSize: this.fileSize,
+    };
+  }
+}
