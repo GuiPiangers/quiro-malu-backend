ebb7ffb8e66a67ac7cf1612d48b0b075251ce03c
Guilherme <eduardomartinspiangers@gmail.com>
2025-01-17 12:54:19 +0000

    feat: create an interface contract to Exams repositories

---
+++ src/repositories/examsFileRepository/IExamsFileRepository.ts
@@ -0,0 +1,32 @@
+import { ExamDTO } from "../../core/exams/models/Exam";
+
+export type saveExamRepositoryRepositoryProps = {
+  userId: string;
+  id?: string;
+  patientId: string;
+  fileName: string;
+  fileType: string;
+  fileSize?: number;
+};
+export type getExamRepositoryProps = {
+  userId: string;
+  patientId: string;
+  id: string;
+};
+
+export type listExamRepositoryProps = {
+  userId: string;
+  patientId: string;
+};
+
+export type deleteExamRepositoryProps = {
+  userId: string;
+  id: string;
+};
+
+export interface IExamsFileRepository {
+  save(data: saveExamRepositoryRepositoryProps): Promise<void>;
+  get(data: getExamRepositoryProps): Promise<ExamDTO>;
+  list(data: listExamRepositoryProps): Promise<ExamDTO[]>;
+  delete(data: deleteExamRepositoryProps): Promise<void>;
+}
