5536dd685afb3b3d859a5721a48acf8a5b018a56
Guilherme <eduardomartinspiangers@gmail.com>
2025-01-17 13:17:07 +0000

    feat: create migration for exams data table

---
+++ src/database/migrations/20250117130426_create_exams.ts
@@ -0,0 +1,30 @@
+import type { Knex } from "knex";
+import { ETableNames } from "../ETableNames";
+
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable(ETableNames.EXAMS, (table) => {
+    table.string("id", 100).primary().index();
+    table.string("userId", 100).index().notNullable();
+    table.string("patientId", 100).index().notNullable();
+    table.string("fileType", 15).notNullable();
+    table.string("fileName", 100).index().notNullable();
+    table.string("fileSize", 25);
+    table.timestamps(true, true);
+    table
+      .foreign("userId")
+      .references("id")
+      .inTable(ETableNames.USERS)
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE");
+    table
+      .foreign("patientId")
+      .references("id")
+      .inTable(ETableNames.PATIENTS)
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE");
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTable(ETableNames.FINANCES);
+}
