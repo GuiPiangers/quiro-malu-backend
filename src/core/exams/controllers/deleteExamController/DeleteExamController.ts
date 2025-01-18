e27cdaa784bd401b10d062cd1945fd715e5f2514
Guilherme Piangers <eduardomartinspiangers@gmail.com>
2025-01-17 21:31:57 -0300

    feat: create DeleteExamController

---
+++ src/core/exams/controllers/deleteExamController/DeleteExamController.ts
@@ -0,0 +1,31 @@
+import { Request, Response } from "express";
+import { ApiError } from "../../../../utils/ApiError";
+import { responseError } from "../../../../utils/ResponseError";
+import { DeleteExamUseCase } from "../../useCases/DeleteExamUSeCase";
+
+export class DeleteExamController {
+  constructor(private deleteExamUseCase: DeleteExamUseCase) {}
+
+  async handle(request: Request, response: Response) {
+    try {
+      const { id: userId } = request.user;
+      const { patientId, id } = request.params as {
+        patientId: string;
+        id: string;
+      };
+
+      if (!userId) throw new ApiError("Acesso não autorizado", 401);
+      if (!id) throw new ApiError("Nenhum paciente enviado", 400);
+
+      await this.deleteExamUseCase.execute({
+        patientId,
+        userId,
+        id,
+      });
+
+      response.status(200).json({ message: "Exame deletado com sucesso" });
+    } catch (error: any) {
+      responseError(response, error);
+    }
+  }
+}
