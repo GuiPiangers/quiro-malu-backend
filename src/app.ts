feaa3242e1f0d1f01b537a7c11ff6bf61747d572
Guilherme <eduardomartinspiangers@gmail.com>
2025-01-27 14:11:39 +0000

    feat: add response time metrics to prometheus collect

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,18 @@
 import "express-async-errors";
-import express from "express";
+import express, { Request, Response } from "express";
 import { router } from "./router";
 import cors from "cors";
 import client from "prom-client";
+import responseTime from "response-time";
 
 const register = new client.Registry();
 client.collectDefaultMetrics({ register });
+
+const restResponseTimeHistogram = new client.Histogram({
+  name: "rest_response_time_duration_seconds",
+  help: "REST API response time in seconds",
+  labelNames: ["method", "route", "status_code"],
+});
 
 const app = express();
 
@@ -25,6 +32,21 @@ app.use((req, res, next) => {
   app.use(cors());
   next();
 });
+
+app.use(
+  responseTime((req: Request, res: Response, time) => {
+    if (req?.route?.path) {
+      restResponseTimeHistogram.observe(
+        {
+          method: req.method,
+          route: req.route.path,
+          status_code: res.statusCode,
+        },
+        time * 1000,
+      );
+    }
+  }),
+);
 
 app.get("/metrics", async (req, res) => {
   res.set("Content-Type", register.contentType);
