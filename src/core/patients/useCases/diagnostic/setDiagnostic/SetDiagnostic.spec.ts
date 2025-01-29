2d8532edb307546b404366628d2aec96b14f59a3
Guilherme <eduardomartinspiangers@gmail.com>
2025-01-29 18:33:28 +0000

    test: create tests to SetDiagnosticUseCase

---
+++ src/core/patients/useCases/diagnostic/setDiagnostic/SetDiagnostic.spec.ts
@@ -0,0 +1,109 @@
+import { mockDiagnosticRepository } from "../../../../../repositories/_mocks/DiagnosticRepositoryMock";
+import { SetDiagnosticUseCase } from "./SetDiagnosticUseCase";
+
+describe("setDiagnosticUseCase", () => {
+  let setDiagnosticUseCase: SetDiagnosticUseCase;
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+    setDiagnosticUseCase = new SetDiagnosticUseCase(mockDiagnosticRepository);
+  });
+
+  describe("execute", () => {
+    it("should call the repository Save method with the correct Data if diagnostic does not exist", async () => {
+      const patientId = "test-patient-id";
+      const userId = "test-user-id";
+      const diagnostic = "test-diagnostic";
+      const treatmentPlan = "test-treatment-plan";
+
+      mockDiagnosticRepository.get.mockResolvedValue(undefined as any);
+
+      await setDiagnosticUseCase.execute(
+        { patientId, diagnostic, treatmentPlan },
+        userId,
+      );
+
+      expect(mockDiagnosticRepository.save).toHaveBeenCalledTimes(1);
+      expect(mockDiagnosticRepository.save).toHaveBeenCalledWith(
+        {
+          patientId,
+          diagnostic,
+          treatmentPlan,
+        },
+        userId,
+      );
+    });
+
+    it("should Update data if optional params were not passed", async () => {
+      const patientId = "test-patient-id";
+      const userId = "test-user-id";
+
+      mockDiagnosticRepository.get.mockResolvedValue({ patientId });
+
+      await setDiagnosticUseCase.execute({ patientId }, userId);
+
+      expect(mockDiagnosticRepository.update).toHaveBeenCalledTimes(1);
+      expect(mockDiagnosticRepository.update).toHaveBeenCalledWith(
+        {
+          patientId,
+        },
+        userId,
+      );
+    });
+
+    it("should Save data if optional params were not passed", async () => {
+      const patientId = "test-patient-id";
+      const userId = "test-user-id";
+
+      mockDiagnosticRepository.get.mockResolvedValue(undefined as any);
+
+      await setDiagnosticUseCase.execute({ patientId }, userId);
+
+      expect(mockDiagnosticRepository.save).toHaveBeenCalledTimes(1);
+      expect(mockDiagnosticRepository.save).toHaveBeenCalledWith(
+        {
+          patientId,
+        },
+        userId,
+      );
+    });
+
+    it("should call the repository Update method with the correct Data if diagnostic does not exist", async () => {
+      const patientId = "test-patient-id";
+      const userId = "test-user-id";
+      const diagnostic = "test-diagnostic";
+      const treatmentPlan = "test-treatment-plan";
+
+      mockDiagnosticRepository.get.mockResolvedValue({ patientId });
+
+      await setDiagnosticUseCase.execute(
+        { patientId, diagnostic, treatmentPlan },
+        userId,
+      );
+
+      expect(mockDiagnosticRepository.update).toHaveBeenCalledTimes(1);
+      expect(mockDiagnosticRepository.update).toHaveBeenCalledWith(
+        {
+          patientId,
+          diagnostic,
+          treatmentPlan,
+        },
+        userId,
+      );
+    });
+
+    it("should propagate an error if the repository set method throws", async () => {
+      const patientId = "test-patient-id";
+      const userId = "test-user-id";
+      const errorMessage = "Failed to set patient";
+
+      mockDiagnosticRepository.get.mockRejectedValueOnce(
+        new Error(errorMessage),
+      );
+
+      await expect(
+        setDiagnosticUseCase.execute({ patientId }, userId),
+      ).rejects.toThrow(errorMessage);
+    });
+  });
+});
--- src/core/patients/useCases/diagnostic/setDiagnostic/SetDiagnosticUseCase.ts
+++ src/core/patients/useCases/diagnostic/setDiagnostic/SetDiagnosticUseCase.ts
@@ -1,20 +1,15 @@
 import { Diagnostic, DiagnosticDTO } from "../../../models/Diagnostic";
 import { IDiagnosticRepository } from "../../../../../repositories/diagnostic/IDiagnosticRepository";
-import { ApiError } from "../../../../../utils/ApiError";
 
 export class SetDiagnosticUseCase {
   constructor(private DiagnosticRepository: IDiagnosticRepository) {}
   async execute(data: DiagnosticDTO, userId: string) {
     const { patientId, diagnostic, treatmentPlan } = new Diagnostic(data);
+
     const diagnosticAlreadyExist = await this.DiagnosticRepository.get(
       data.patientId,
       userId,
     );
-
-    if (!patientId)
-      throw new ApiError(
-        "Deve ser informado o patientId, diagnostic e treatmentPlan",
-      );
 
     if (diagnosticAlreadyExist) {
       await this.DiagnosticRepository.update(
