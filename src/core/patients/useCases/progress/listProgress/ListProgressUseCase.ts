0b95ce4ff70a387bd2b79e7e3ccfb1a449d7eb7c
Guilherme <eduardomartinspiangers@gmail.com>
2025-01-30 12:05:03 +0000

    refactor: fix prettier errors on ListProgressUseCase

--- src/core/patients/useCases/progress/listProgress/ListProgressUseCase.ts
+++ src/core/patients/useCases/progress/listProgress/ListProgressUseCase.ts
@@ -1,19 +1,28 @@
 import { IProgressRepository } from "../../../../../repositories/progress/IProgressRepository";
 
 export class ListProgressUseCase {
-    constructor(
-        private ProgressRepository: IProgressRepository
-    ) { }
+  constructor(private ProgressRepository: IProgressRepository) {}
 
-    async execute({ patientId, userId, page }: { patientId: string, userId: string, page?: number }) {
-        const limit = 10
-        const offSet = page ? limit * (page - 1) : 0
+  async execute({
+    patientId,
+    userId,
+    page,
+  }: {
+    patientId: string;
+    userId: string;
+    page?: number;
+  }) {
+    const limit = 10;
+    const offSet = page ? limit * (page - 1) : 0;
 
+    const progressData = this.ProgressRepository.list({
+      patientId,
+      userId,
+      config: { limit, offSet },
+    });
+    const totalProgress = this.ProgressRepository.count({ patientId, userId });
 
-        const progressData = this.ProgressRepository.list({ patientId, userId, config: { limit, offSet } })
-        const totalProgress = this.ProgressRepository.count({ patientId, userId })
-
-        const [progress, total] = await Promise.all([progressData, totalProgress])
-        return { progress, total: total[0]['total'], limit }
-    }
+    const [progress, total] = await Promise.all([progressData, totalProgress]);
+    return { progress, total: total[0].total, limit };
+  }
 }
