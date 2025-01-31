f0a4745e656d6e6d7b25876f51edbc5ab8e8b0b4
Guilherme <eduardomartinspiangers@gmail.com>
2025-01-31 18:58:27 +0000

    refactor: fix prettier errors of DeleteServiceUseCase

--- src/core/service/useCases/deleteService/DeleteServiceUseCase.ts
+++ src/core/service/useCases/deleteService/DeleteServiceUseCase.ts
@@ -1,11 +1,9 @@
 import { IServiceRepository } from "../../../../repositories/service/IServiceRepository";
 
 export class DeleteServiceUseCase {
-    constructor(
-        private serviceRepository: IServiceRepository,
-    ) { }
+  constructor(private serviceRepository: IServiceRepository) {}
 
-    async execute({ id, userId }: { id: string, userId: string }) {
-        await this.serviceRepository.delete({ id, userId })
-    }
+  async execute({ id, userId }: { id: string; userId: string }) {
+    await this.serviceRepository.delete({ id, userId });
+  }
 }
