94e5f913327147df83e171fd5ae4a9423ab8f7ec
Guilherme <eduardomartinspiangers@gmail.com>
2025-02-03 19:36:56 +0000

    test: create FinanceRepositoryMock

---
+++ src/repositories/_mocks/FinanceRepositoryMock.ts
@@ -0,0 +1,10 @@
+import { IFinanceRepository } from "../finance/IFinanceRepository";
+
+export const mockFinanceRepository: jest.Mocked<IFinanceRepository> = {
+  create: jest.fn(),
+  update: jest.fn(),
+  list: jest.fn(),
+  get: jest.fn(),
+  getByScheduling: jest.fn(),
+  delete: jest.fn(),
+};
