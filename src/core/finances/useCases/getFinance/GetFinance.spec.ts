20f62335e6871a9e7afb0f9ca624df14334059e6
Guilherme <eduardomartinspiangers@gmail.com>
2025-02-03 19:37:41 +0000

    test: create tests for GetFinanceUseCase

---
+++ src/core/finances/useCases/getFinance/GetFinance.spec.ts
@@ -0,0 +1,69 @@
+import { mockFinanceRepository } from "../../../../repositories/_mocks/FinanceRepositoryMock";
+import { FinanceDTO } from "../../models/Finance";
+import { GetFinanceUseCase } from "./getFinanceUseCase";
+
+describe("getFinanceUseCase", () => {
+  let getFinanceUseCase: GetFinanceUseCase;
+
+  describe("execute", () => {
+    beforeEach(() => {
+      jest.clearAllMocks();
+      getFinanceUseCase = new GetFinanceUseCase(mockFinanceRepository);
+    });
+
+    it("Should return Finance data", async () => {
+      const userId = "test-user-id";
+      const FinanceId = "test-Finance-id";
+      const FinanceData: FinanceDTO = {
+        description: "Quiropraxia",
+        value: 100,
+        date: "2025/01/10",
+        type: "expense",
+        paymentMethod: "money",
+      };
+
+      mockFinanceRepository.get.mockResolvedValue(FinanceData);
+
+      const result = await getFinanceUseCase.execute({ id: FinanceId, userId });
+
+      expect(result).toEqual(FinanceData);
+    });
+
+    it("Should call repository get method with correct arguments ", async () => {
+      const userId = "test-user-id";
+      const FinanceId = "test-Finance-id";
+      const FinanceData: FinanceDTO = {
+        description: "Quiropraxia",
+        value: 100,
+        date: "2025/01/10",
+        type: "expense",
+        paymentMethod: "money",
+      };
+
+      mockFinanceRepository.get.mockResolvedValue(FinanceData);
+
+      await getFinanceUseCase.execute({
+        userId,
+        id: FinanceId,
+      });
+
+      expect(mockFinanceRepository.get).toHaveBeenCalledWith({
+        userId,
+        id: FinanceId,
+      });
+      expect(mockFinanceRepository.get).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  it("Should throw an Error if repository get method throws", async () => {
+    const userId = "test-user-id";
+    const FinanceId = "test-Finance-id";
+    const errorMessage = "Error getting Finance";
+
+    mockFinanceRepository.get.mockRejectedValue(new Error(errorMessage));
+
+    await expect(
+      getFinanceUseCase.execute({ id: FinanceId, userId }),
+    ).rejects.toThrow(errorMessage);
+  });
+});
