0f5f65be9aaf5c18888d6f55d52f53fb93671140
Guilherme <eduardomartinspiangers@gmail.com>
2025-02-04 11:16:45 +0000

    test: create test for ListFinanceUseCase

---
+++ src/core/finances/useCases/listFinances/ListFinance.spec.ts
@@ -0,0 +1,89 @@
+import { mockFinanceRepository } from "../../../../repositories/_mocks/FinanceRepositoryMock";
+import { ApiError } from "../../../../utils/ApiError";
+import { FinanceDTO } from "../../models/Finance";
+import { ListFinancesUseCase } from "./listFinancesUseCase";
+
+describe("listFinanceUseCase", () => {
+  let listFinancesUseCase: ListFinancesUseCase;
+
+  describe("execute", () => {
+    beforeEach(() => {
+      jest.clearAllMocks();
+      listFinancesUseCase = new ListFinancesUseCase(mockFinanceRepository);
+    });
+
+    it("Should return Finance data", async () => {
+      const userId = "test-user-id";
+      const yearAndMonth = "2025-01";
+
+      const financeData: FinanceDTO = {
+        description: "Quiropraxia",
+        value: 100,
+        date: "2025/01/10",
+        type: "expense",
+        paymentMethod: "money",
+      };
+
+      mockFinanceRepository.list.mockResolvedValue([financeData]);
+
+      const result = await listFinancesUseCase.execute({
+        userId,
+        yearAndMonth,
+      });
+
+      expect(result).toEqual([financeData]);
+    });
+
+    it("Should call repository list method with correct arguments ", async () => {
+      const userId = "test-user-id";
+      const yearAndMonth = "2025-01";
+      const financeData: FinanceDTO = {
+        description: "Quiropraxia",
+        value: 100,
+        date: "2025/01/10",
+        type: "expense",
+        paymentMethod: "money",
+      };
+
+      mockFinanceRepository.list.mockResolvedValue([financeData]);
+
+      await listFinancesUseCase.execute({
+        userId,
+        yearAndMonth,
+      });
+
+      expect(mockFinanceRepository.list).toHaveBeenCalledWith({
+        userId,
+        yearAndMonth,
+      });
+      expect(mockFinanceRepository.list).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  it("Should throw an ApiError if yearAndDate param is invalid", async () => {
+    const userId = "test-user-id";
+    const yearAndMonth = "2025/01";
+
+    await expect(
+      listFinancesUseCase.execute({ userId, yearAndMonth }),
+    ).rejects.toThrow(ApiError);
+
+    await expect(
+      listFinancesUseCase.execute({ userId, yearAndMonth }),
+    ).rejects.toThrow(
+      "Formato de yearAndMonth inválido. O formato deve ser YYYY-MM",
+    );
+  });
+
+  it("Should throw an Error if repository list method throws", async () => {
+    const userId = "test-user-id";
+    const yearAndMonth = "2025-01";
+    const errorMessage = "Error listing Finance";
+
+    mockFinanceRepository.list.mockRejectedValue(new Error(errorMessage));
+
+    await expect(
+      listFinancesUseCase.execute({ userId, yearAndMonth }),
+    ).rejects.toThrow(errorMessage);
+  });
+});
--- src/core/finances/useCases/listFinances/listFinancesUseCase.ts
+++ src/core/finances/useCases/listFinances/listFinancesUseCase.ts
@@ -2,15 +2,28 @@ import {
   IFinanceRepository,
   listFinanceProps,
 } from "../../../../repositories/finance/IFinanceRepository";
+import { ApiError } from "../../../../utils/ApiError";
 
 export class ListFinancesUseCase {
   constructor(private financeRepository: IFinanceRepository) {}
 
   async execute({ userId, yearAndMonth, config }: listFinanceProps) {
+    this.validateYearAndMonth(yearAndMonth);
+
     return await this.financeRepository.list({
       userId,
       yearAndMonth,
       config,
     });
+  }
+
+  private validateYearAndMonth(yearAndMonth: string) {
+    const vaidTemplate = /^\d{4}-\d{2}$/;
+    if (!vaidTemplate.test(yearAndMonth)) {
+      throw new ApiError(
+        "Formato de yearAndMonth inválido. O formato deve ser YYYY-MM",
+        400,
+      );
+    }
   }
 }
