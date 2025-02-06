e0f37c40ede48a1a5b30939162eff879924159ca
Guilherme Piangers <eduardomartinspiangers@gmail.com>
2025-02-06 20:55:07 -0300

    test: create mock for NotificationRepository

---
+++ src/repositories/_mocks/NotificationRepositoryMock.ts
@@ -0,0 +1,10 @@
+import { INotificationRepository } from "../notification/INotificationRepository";
+
+export const createMockNotificationRepository =
+  (): jest.Mocked<INotificationRepository> => ({
+    delete: jest.fn(),
+    get: jest.fn(),
+    list: jest.fn(),
+    save: jest.fn(),
+    update: jest.fn(),
+  });
