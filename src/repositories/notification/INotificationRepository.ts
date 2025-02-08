9475180874c9cd43124b619fe5cb8a46c1fd6b45
Guilherme Piangers <eduardomartinspiangers@gmail.com>
2025-02-08 08:23:15 -0300

    feat: add countNotRead method to INotificationRepository

--- src/repositories/notification/INotificationRepository.ts
+++ src/repositories/notification/INotificationRepository.ts
@@ -1,23 +1,27 @@
-import { NotificationDOT } from "../../core/notification/models/Notification";
+import { NotificationDTO } from "../../core/notification/models/Notification";
 
-export type SaveNotificationParams = NotificationDOT & {
+export type SaveNotificationParams = NotificationDTO & {
   id: string;
   userId: string;
 };
 
-export type UpdateNotificationParams = NotificationDOT & {
+export type UpdateNotificationParams = NotificationDTO & {
   id: string;
   userId: string;
 };
 
-export type GetNotificationParams = NotificationDOT & {
+export type GetNotificationParams = {
   id: string;
   userId: string;
 };
 
-export type ListNotificationParams = NotificationDOT & {
+export type ListNotificationParams = {
   userId: string;
   config?: { offset: number; limit: number };
+};
+
+export type CountNotReadNotificationParams = {
+  userId: string;
 };
 
 export type DeleteNotificationParams = { userId: string; id: string };
@@ -28,4 +32,5 @@ export interface INotificationRepository {
   update(data: UpdateNotificationParams): Promise<void>;
   get(data: GetNotificationParams): Promise<void>;
   list(data: ListNotificationParams): Promise<void>;
+  countNotRead(data: CountNotReadNotificationParams): Promise<number>;
 }
