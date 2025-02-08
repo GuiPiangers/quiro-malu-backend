ae5cd903265f87b255ef90d296fea8313cc771a8
Guilherme Piangers <eduardomartinspiangers@gmail.com>
2025-02-08 13:44:26 -0300

    fix: change headers and message JSON od sentNotificationController

--- src/core/notification/controllers/sentNotification/sentNotificationController.ts
+++ src/core/notification/controllers/sentNotification/sentNotificationController.ts
@@ -10,19 +10,32 @@ import {
 export class sentNotificationController {
   async handle(request: Request, response: Response) {
     try {
-      response.set({
-        "Content-Type": "text/event-stream",
-        Connection: "keep-alive",
-      });
+      response.setHeader("Content-Type", "text/event-stream");
+      response.setHeader("Cache-Control", "no-cache");
+      response.setHeader("Connection", "keep-alive");
 
       const sentNotificationObserver: NotificationObserverEvent = async (
         notification: NotificationDTO,
         totalNotRead: number,
       ) => {
-        response.write(JSON.stringify({ notification, totalNotRead }));
+        response.write(
+          `data: ${JSON.stringify({ notification, totalNotRead })}\n\n`,
+        );
       };
-
       notificationObserver.addObserver(sentNotificationObserver);
+
+      const interval = setInterval(() => {
+        const eventData = {
+          timestamp: new Date().toISOString(),
+          message: "Novo evento!",
+        };
+        response.write(`data: ${JSON.stringify(eventData)}\n\n`);
+      }, 3000);
+
+      request.on("close", () => {
+        clearInterval(interval);
+        response.end();
+      });
     } catch (err: any) {
       return responseError(response, err);
     }
