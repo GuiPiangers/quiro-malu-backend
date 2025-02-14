4f62d421e01d74ca06e3afa98bf227b01c0c9da6
Guilherme Piangers <eduardomartinspiangers@gmail.com>
2025-02-13 21:04:00 -0300

    feat: create PushNotificationQueue

---
+++ src/database/bull/pushNotifications/pushNotificationsQueue.ts
@@ -0,0 +1,30 @@
+import { Queue } from "bullmq";
+import IORedis from "ioredis";
+import { NotificationDTO } from "../../../core/notification/models/Notification";
+
+const host = process.env.DB_HOST;
+const redisPassword = process.env.REDIS_PASSWORD;
+
+const connection = new IORedis({ host, password: redisPassword });
+
+export class PushNotificationQueue {
+  private pushNotificationQueue = new Queue("pushNotification", { connection });
+
+  async add({
+    delay,
+    notification,
+    userId,
+  }: {
+    notification: NotificationDTO;
+    userId: string;
+    delay: number;
+  }) {
+    await this.pushNotificationQueue.add(
+      "notifyUser",
+      { notification, userId },
+      { delay },
+    );
+  }
+}
+
+export const pushNotificationQueue = new PushNotificationQueue();
