ab536f8d37f467640171415589210c4906b41ae4
Guilherme Piangers <eduardomartinspiangers@gmail.com>
2025-02-15 10:54:13 -0300

    feat: emit delete event when delete schedule

--- src/core/scheduling/useCases/deleteScheduling/DeleteSchedulingUseCase.ts
+++ src/core/scheduling/useCases/deleteScheduling/DeleteSchedulingUseCase.ts
@@ -1,9 +1,11 @@
 import { ISchedulingRepository } from "../../../../repositories/scheduling/ISchedulingRepository";
+import { schedulingObserver } from "../../../shared/observers/SchedulingObserver/SchedulingObserver";
 
 export class DeleteSchedulingUseCase {
   constructor(private SchedulingRepository: ISchedulingRepository) {}
 
   async execute({ id, userId }: { id: string; userId: string }) {
     await this.SchedulingRepository.delete({ id, userId });
+    schedulingObserver.emit("delete", { id, userId, patientId: "" });
   }
 }
