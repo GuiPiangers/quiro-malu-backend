9120213e84f5a4326a97521970244f2671392924
Guilherme Piangers <eduardomartinspiangers@gmail.com>
2025-02-15 09:00:47 -0300

    feat: create worker for pushNotificationQueue

--- src/database/bull/pushNotifications/pushNotificationsQueue.ts
+++ src/database/bull/pushNotifications/pushNotificationsQueue.ts
@@ -1,25 +1,25 @@
-import { Queue } from "bullmq";
+import { Queue, Worker } from "bullmq";
 import IORedis from "ioredis";
 import { NotificationDTO } from "../../../core/notification/models/Notification";
-import { Crypto } from "../../../core/shared/helpers/Crypto";
+import { sendPushNotificationUseCase } from "../../../core/notification/useCases/sendPushNotification";
+import {
+  IPushNotificationQueue,
+  PushNotificationQueuePrams,
+} from "./IPushNotificationQueue";
 
 const host = process.env.DB_HOST;
 const redisPassword = process.env.REDIS_PASSWORD;
 
-const connection = new IORedis({ host, password: redisPassword });
+const connection = new IORedis({
+  host,
+  password: redisPassword,
+  maxRetriesPerRequest: null,
+});
 
-export class PushNotificationQueue {
+export class PushNotificationQueue implements IPushNotificationQueue {
   private pushNotificationQueue = new Queue("pushNotification", { connection });
 
-  async add({
-    delay,
-    notification,
-    userId,
-  }: {
-    notification: NotificationDTO;
-    userId: string;
-    delay: number;
-  }) {
+  async add({ delay, notification, userId }: PushNotificationQueuePrams) {
     await this.pushNotificationQueue.add(
       "notifyUser",
       { notification, userId },
@@ -32,4 +32,20 @@ export class PushNotificationQueue {
   }
 }
 
-export const pushNotificationQueue = new PushNotificationQueue();
+const pushNotificationQueue = new PushNotificationQueue();
+
+const worker = new Worker(
+  "pushNotification",
+  async (job) => {
+    const data = job.data as { notification: NotificationDTO; userId: string };
+    sendPushNotificationUseCase.execute({
+      notificationData: data.notification,
+      userId: data.userId,
+    });
+  },
+  { connection },
+);
+
+worker.on("error", (error) => console.log(error));
+
+export { pushNotificationQueue };
