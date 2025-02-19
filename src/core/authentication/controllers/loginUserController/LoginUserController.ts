6e6713e7cec176bf5b232aedb859f3c6c8509a31
Guilherme Piangers <eduardomartinspiangers@gmail.com>
2025-02-19 19:30:56 -0300

    chore: install cookie-parser

--- src/core/authentication/controllers/loginUserController/LoginUserController.ts
+++ src/core/authentication/controllers/loginUserController/LoginUserController.ts
@@ -3,16 +3,19 @@ import { LoginUserUseCase } from "../..//useCases/loginUser/LoginUserUseCase";
 import { responseError } from "../../../../utils/ResponseError";
 
 export class LoginUserController {
-    constructor(private loginUserUseCase: LoginUserUseCase) { }
+  constructor(private loginUserUseCase: LoginUserUseCase) {}
 
-    async handle(request: Request, response: Response) {
-        try {
-            const { email, password } = request.body
-            const { token, refreshToken, user } = await this.loginUserUseCase.execute(email, password)
+  async handle(request: Request, response: Response) {
+    try {
+      const { email, password } = request.body;
+      const { token, refreshToken, user } = await this.loginUserUseCase.execute(
+        email,
+        password,
+      );
 
-            return response.json({ token, refreshToken, user }).status(200)
-        } catch (err: any) {
-            return responseError(response, err)
-        }
+      return response.json({ token, refreshToken, user }).status(200);
+    } catch (err: any) {
+      return responseError(response, err);
     }
+  }
 }
