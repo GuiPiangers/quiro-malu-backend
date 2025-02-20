2ea2feb5509b98bb7d70d190ba9666b17037fabb
Guilherme <eduardomartinspiangers@gmail.com>
2025-02-20 11:48:36 +0000

    feat: add method toLocaleDate to DateTime class

--- src/core/shared/Date.ts
+++ src/core/shared/Date.ts
@@ -77,6 +77,10 @@ export class DateTime {
     return date.replace(/(\d{2})\/(\d{2})\/(\d{4})/, "$3-$2-$1");
   }
 
+  static toLocaleDate(date: string) {
+    return date.replace(/(\d{4})-(\d{2})-(\d{2})/, "$3/$2/$1");
+  }
+
   static now(props: DateTimeConfig = {}) {
     return new DateTime(new Date().toISOString(), props);
   }
