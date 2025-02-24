3e10f5d6896cee3612e3e5fdaac358af51b616e5
Guilherme <eduardomartinspiangers@gmail.com>
2025-02-24 19:35:56 +0000

    fix: instantiation of class SendNotificationController

--- src/core/notification/controllers/sendNotification/index.ts
+++ src/core/notification/controllers/sendNotification/index.ts
@@ -1,5 +1,14 @@
+import { MongoNotificationRepository } from "../../../../repositories/notification/MongoNotificationRepository";
+import SendAppNotificationUseCase from "../../useCases/sendNotificationUseCase";
 import { SendNotificationController } from "./sendNotificationController";
 
-const sendNotificationController = new SendNotificationController();
+const notificationRepository = new MongoNotificationRepository();
+const sendNotificationUseCase = new SendAppNotificationUseCase(
+  notificationRepository,
+);
+
+const sendNotificationController = new SendNotificationController(
+  sendNotificationUseCase,
+);
 
 export { sendNotificationController };
