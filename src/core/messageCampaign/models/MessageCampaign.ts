7b3a904430b4ab0ee07a895043cb6572c834c00e
Guilherme Piangers <eduardomartinspiangers@gmail.com>
2025-02-28 22:13:46 -0300

    feat: create MessageCampaign Model

--- src/core/messageCampaign/models/MessageCampaign.ts
+++ src/core/messageCampaign/models/MessageCampaign.ts
@@ -1,3 +1,5 @@
+import { Entity } from "../../shared/Entity";
+
 export type MessageCampaignDTO = {
   id?: string;
   name: string;
@@ -6,3 +8,39 @@ export type MessageCampaignDTO = {
   initialDate?: string;
   endDate?: string;
 };
+
+export class MessageCampaign extends Entity {
+  readonly name: string;
+  readonly templateMessage: string;
+  readonly active: boolean;
+  readonly initialDate?: string;
+  readonly endDate?: string;
+
+  constructor({
+    active,
+    name,
+    templateMessage,
+    endDate,
+    id,
+    initialDate,
+  }: MessageCampaignDTO) {
+    super(id);
+
+    this.name = name;
+    this.templateMessage = templateMessage;
+    this.active = active;
+    this.initialDate = initialDate;
+    this.endDate = endDate;
+  }
+
+  getDTO() {
+    return {
+      active: this.active,
+      name: this.name,
+      templateMessage: this.templateMessage,
+      endDate: this.endDate,
+      id: this.id,
+      initialDate: this.initialDate,
+    };
+  }
+}
