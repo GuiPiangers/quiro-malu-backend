89896a5515fa46e8c3c7c0d1aef7fb96e02dc732
Guilherme <eduardomartinspiangers@gmail.com>
2025-03-03 14:01:13 +0000

    feat: create GetPatientsByBirthDayUseCase

---
+++ src/core/patients/useCases/getBytBirthDay/GetByBirthDayUseCase.ts
@@ -0,0 +1,20 @@
+import { IPatientRepository } from "../../../../repositories/patient/IPatientRepository";
+
+export class GetPatientsByBirthDayUseCase {
+  constructor(private patientRepository: IPatientRepository) {}
+
+  async execute({
+    dateOfBirth,
+    userId,
+  }: {
+    dateOfBirth: string;
+    userId: string;
+  }) {
+    const patients = await this.patientRepository.getByDateOfBirth({
+      dateOfBirth,
+      userId,
+    });
+
+    return patients;
+  }
+}
