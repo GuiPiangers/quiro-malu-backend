ebbe274d1fba636979ad6310f9030eb976f75c6d
Guilherme <eduardomartinspiangers@gmail.com>
2025-03-03 20:15:07 +0000

    feat: create PatientsBirthDayQueue

---
+++ src/database/bull/patientBirthDay/patientsBirthDayQueue.ts
@@ -0,0 +1,36 @@
+import { Queue, Worker } from "bullmq";
+
+import { redis } from "../../redis";
+import {
+  IPatientsBirthDayQueue,
+  PatientsBirthDayQueuePrams,
+} from "./IPatientBirthDayQueue";
+import { IPatientRepository } from "../../../repositories/patient/IPatientRepository";
+
+const connection = redis;
+
+export class PatientsBirthDayQueue implements IPatientsBirthDayQueue {
+  private patientsBirthQueue = new Queue("patientsBirth", { connection });
+
+  async add({ patientRepository }: PatientsBirthDayQueuePrams) {
+    await this.patientsBirthQueue.upsertJobScheduler(
+      "patientsBirthDay",
+      { pattern: "0 0 5 * * *" },
+      { data: patientRepository },
+    );
+  }
+}
+
+const patientsBirthDayQueue = new PatientsBirthDayQueue();
+
+const worker = new Worker(
+  "patientsBirth",
+  async (job) => {
+    const patientRepository = job.data as IPatientRepository;
+  },
+  { connection },
+);
+
+worker.on("error", (error) => console.log(error));
+
+export { patientsBirthDayQueue as pushNotificationQueue };
