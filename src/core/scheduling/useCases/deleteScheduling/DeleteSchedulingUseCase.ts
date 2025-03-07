455eae221879e232cf6275bf625feb9578f4ea46
Guilherme Piangers <eduardomartinspiangers@gmail.com>
2025-03-07 20:47:21 -0300

    refactor: user  AppEventListener on DeleteSchedulingUseCase

--- src/core/scheduling/useCases/deleteScheduling/DeleteSchedulingUseCase.ts
+++ src/core/scheduling/useCases/deleteScheduling/DeleteSchedulingUseCase.ts
@@ -1,11 +1,11 @@
 import { ISchedulingRepository } from "../../../../repositories/scheduling/ISchedulingRepository";
-import { schedulingObserver } from "../../../shared/observers/SchedulingObserver/SchedulingObserver";
+import { appEventListener } from "../../../shared/observers/EventListener";
 
 export class DeleteSchedulingUseCase {
   constructor(private SchedulingRepository: ISchedulingRepository) {}
 
   async execute({ id, userId }: { id: string; userId: string }) {
     await this.SchedulingRepository.delete({ id, userId });
-    schedulingObserver.emit("delete", { id, userId, patientId: "" });
+    appEventListener.emit("deleteSchedule", { id, userId });
   }
 }
