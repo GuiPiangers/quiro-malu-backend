e085b398c208dd58a31bb369e1992b10fd7b3157
Guilherme Piangers <eduardomartinspiangers@gmail.com>
2025-03-07 20:44:29 -0300

    feat: add create, update, delete exam types to appEvents on AppEventListener

--- src/core/shared/observers/EventListener.ts
+++ src/core/shared/observers/EventListener.ts
@@ -1,14 +1,19 @@
+import { ExamDTO } from "../../exams/models/Exam";
 import { PatientDTO } from "../../patients/models/Patient";
 import { SchedulingDTO } from "../../scheduling/models/Scheduling";
 
 type AppEvents = {
   createPatient: PatientDTO & { userId: string };
   updatePatient: Partial<PatientDTO> & { id: string; userId: string };
-  deletePatient: { userId: string; patientId: string } & { userId: string };
+  deletePatient: { userId: string; id: string };
 
   createSchedule: SchedulingDTO & { userId: string };
   updateSchedule: Partial<SchedulingDTO> & { id: string; userId: string };
-  deleteSchedule: { schedulingId: string; userId: string };
+  deleteSchedule: { id: string; userId: string };
+
+  createExam: ExamDTO & { userId: string };
+  updateExam: Partial<ExamDTO> & { userId: string; id: string };
+  deleteExam: { userId: string; id: string; patientId: string };
 };
 
 type Listener<T> = (data: T) => Promise<void>;
