4dff80c2598a7053148ff3bd532805293e8a7e51
Guilherme Piangers <eduardomartinspiangers@gmail.com>
2025-03-09 13:11:15 -0300

    refactor: use DateTime.difference on ClientStatusStrategy to return correct status

--- src/core/scheduling/models/status/ClientStatusStrategy.ts
+++ src/core/scheduling/models/status/ClientStatusStrategy.ts
@@ -1,16 +1,16 @@
-import { TZDate } from "@date-fns/tz";
 import { StatusStrategy, StatusStrategyData } from "./StatusStrategy";
 import { ApiError } from "../../../../utils/ApiError";
+import { DateTime } from "../../../shared/Date";
 
 export default class ClientStatusStrategy implements StatusStrategy {
   calculateStatus({ scheduling, status }: StatusStrategyData) {
     if (!scheduling.date?.dateTime)
       throw new ApiError("A data deve ser definida para calcular o status");
 
-    const date = new TZDate(new Date(), "Etc/UTC");
-    const schedulingDate = new TZDate(scheduling.date?.dateTime, "Etc/UTC");
+    const now = DateTime.now();
+    const difference = DateTime.difference(scheduling.date, now);
 
-    if (schedulingDate < date && status === "Agendado") return "Atrasado";
+    if (difference < 0 && status === "Agendado") return "Atrasado";
     return status || "Agendado";
   }
 }
