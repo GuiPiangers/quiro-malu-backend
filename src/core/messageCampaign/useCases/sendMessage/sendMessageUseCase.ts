2430e75b2cefb6b1d13d96cd059e00530c0d5eca
Guilherme Piangers <eduardomartinspiangers@gmail.com>
2025-03-15 08:58:37 -0300

    feat: use replaceVariables method of SendMessageUseCase

--- src/core/messageCampaign/useCases/sendMessage/sendMessageUseCase.ts
+++ src/core/messageCampaign/useCases/sendMessage/sendMessageUseCase.ts
@@ -2,6 +2,10 @@ import { IPatientRepository } from "../../../../repositories/patient/IPatientRep
 import { ISchedulingRepository } from "../../../../repositories/scheduling/ISchedulingRepository";
 import { NotificationSendMessage } from "../../../notification/models/NotificationSendMessage";
 import { sendAndSaveNotificationUseCase } from "../../../notification/useCases/sendAndSaveNotification";
+import { PatientDTO } from "../../../patients/models/Patient";
+import { SchedulingDTO } from "../../../scheduling/models/Scheduling";
+import { calculateAge } from "../../../shared/calculateAge";
+import { DateTime } from "../../../shared/Date";
 import { Phone } from "../../../shared/Phone";
 import {
   MessageCampaign,
@@ -27,6 +31,12 @@ export class SendMessageUseCase {
   }) {
     const message = new MessageCampaign(messageCampaign);
     const [patient] = await this.patientRepository.getById(patientId, userId);
+    const [scheduling] = schedulingId
+      ? await this.schedulingRepository.get({
+          id: schedulingId,
+          userId,
+        })
+      : [];
 
     const notification = new NotificationSendMessage({
       title: `Enviar mensagem para ${patient?.name}`,
@@ -34,9 +44,60 @@ export class SendMessageUseCase {
       params: {
         patientId,
         patientPhone: new Phone(patient.phone),
-        templateMessage: message.templateMessage,
+        templateMessage: this.replaceVariables(message.templateMessage, {
+          patient,
+          scheduling,
+        }),
       },
     });
     sendAndSaveNotificationUseCase.execute({ userId, notification });
+  }
+
+  private replaceVariables(
+    messageTemplate: string,
+    {
+      patient,
+      scheduling,
+    }: {
+      patient: PatientDTO;
+      scheduling?: SchedulingDTO;
+    },
+  ) {
+    const extractRegex = /(\{\{)|(\}\})/g;
+
+    const variableDictionary: Record<string, string> = {
+      nome: patient.name,
+      telefone: patient.phone,
+      genero: patient.gender ?? "",
+      idade: patient.dateOfBirth ? `${calculateAge(patient.dateOfBirth)}` : "",
+      dataConsulta: scheduling?.date
+        ? new DateTime(scheduling?.date).date +
+          " às " +
+          new DateTime(scheduling?.date).time
+        : "",
+      servico: scheduling?.service ?? "",
+      status: scheduling?.status ?? "",
+    };
+
+    const variables = this.extractVariables(messageTemplate);
+
+    const replacedString = variables.reduce((acc, value) => {
+      const valueWithoutBraces = value.replace(extractRegex, "");
+      const result = acc.replace(
+        value,
+        variableDictionary[valueWithoutBraces] as string,
+      );
+      return result;
+    }, messageTemplate);
+
+    return replacedString;
+  }
+
+  private extractVariables(templateMessage: string) {
+    const matchRegex = /\{\{.*?\}\}/g;
+
+    const result = templateMessage.match(matchRegex) ?? [];
+
+    return result;
   }
 }
