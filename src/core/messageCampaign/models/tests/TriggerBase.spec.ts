171613c75d2d103761fd8d737da51f8cb8f70d8a
Guilherme <eduardomartinspiangers@gmail.com>
2025-03-19 19:41:08 +0000

    test: adapt Trigger test to test TriggerBase class

--- src/core/messageCampaign/models/tests/TriggerBase.spec.ts
+++ src/core/messageCampaign/models/tests/TriggerBase.spec.ts
@@ -3,7 +3,7 @@ import { TriggerBase, TriggerDTO } from "../Trigger";
 
 const mockEvent = "createPatient";
 
-describe("Trigger", () => {
+describe("TriggerBase", () => {
   it("deve inicializar corretamente com os valores fornecidos", () => {
     const dto: TriggerDTO = { event: mockEvent };
     const trigger = new TriggerBase(dto);
@@ -11,19 +11,19 @@ describe("Trigger", () => {
     expect(trigger.event).toBe(mockEvent);
   });
 
-  // it("deve definir delayOperatorInMinutes como 0 se não for fornecido", () => {
-  //   const dto: TriggerDTO = { event: mockEvent };
-  //   const trigger = new Trigger(dto);
+  it("deve definir delayOperatorInMinutes como 0 se não for fornecido", () => {
+    const dto: TriggerDTO = { event: mockEvent };
+    const trigger = new TriggerBase(dto);
 
-  //   expect(trigger.delayOperatorInMinutes).toBe(0);
-  // });
+    expect(trigger.calculateDelay({ date: DateTime.now() })).toBe(0);
+  });
 
   it("deve calcular corretamente o delay com um operador positivo", () => {
     const dto: TriggerDTO = { event: mockEvent };
     const trigger = new TriggerBase(dto);
 
     const now = DateTime.now();
-    const delay = trigger.calculateDelay({ date: now });
+    const delay = trigger.calculateDelay({ date: now, delayInMinutes: 10 });
 
     expect(delay).toBeGreaterThan(0);
     expect(delay).toBeCloseTo(10 * 60 * 1000, -10); // Aproximadamente 10 minutos em ms
