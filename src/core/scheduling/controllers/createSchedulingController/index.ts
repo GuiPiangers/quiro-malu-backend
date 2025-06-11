62ea6ace25649ddbe02c705fc08507ef229d0076
guiPiangers <eduardomartinspiangers@gmail.com>
2025-06-10 22:32:20 -0300

    fix: Instanciando a classe CreateSchedulingUseCase com a nova dependencia BlockScheduleRepository ao criar o createSchedulingController

--- src/core/scheduling/controllers/createSchedulingController/index.ts
+++ src/core/scheduling/controllers/createSchedulingController/index.ts
@@ -1,10 +1,13 @@
+import { BlockScheduleRepository } from "../../../../repositories/blockScheduleRepository/BlockScheduleRepository";
 import { MySqlSchedulingRepository } from "../../../../repositories/scheduling/MySqlSchedulingRepository";
 import { CreateSchedulingUseCase } from "../../useCases/createScheduling/CreateSchedulingUseCase";
 import { CreateSchedulingController } from "./CreateSchedulingController";
 
 const schedulingRepository = new MySqlSchedulingRepository();
+const blockSchedulingRepository = new BlockScheduleRepository();
 const createSchedulingUseCase = new CreateSchedulingUseCase(
   schedulingRepository,
+  blockSchedulingRepository,
 );
 const createSchedulingController = new CreateSchedulingController(
   createSchedulingUseCase,
