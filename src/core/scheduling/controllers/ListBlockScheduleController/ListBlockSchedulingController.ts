057bfa4fb9facd7f7344bf9798c87af86b136df6
guiPiangers <eduardomartinspiangers@gmail.com>
2025-08-13 22:09:04 -0300

    fix: Buscando os parametros de startDate e endDate através da url no ListBlockScheduleController

--- src/core/scheduling/controllers/ListBlockScheduleController/ListBlockSchedulingController.ts
+++ src/core/scheduling/controllers/ListBlockScheduleController/ListBlockSchedulingController.ts
@@ -1,21 +1,23 @@
 import { Request, Response } from "express";
 import { responseError } from "../../../../utils/ResponseError";
 import { ListBlockSchedulingUseCase } from "../../useCases/blockScheduling/listBlockSchedules/ListBlockSchedulingUseCase";
+import { ApiError } from "../../../../utils/ApiError";
 
 export class ListBlockScheduleController {
   constructor(private listBlockScheduleUseCase: ListBlockSchedulingUseCase) {}
 
   async handle(request: Request, response: Response) {
     try {
-      const data = request.body as {
-        startDate: string;
-        endDate: string;
-        description?: string;
-      };
+      const { startDate, endDate } = request.query;
       const userId = request.user.id as string;
 
+      if (!startDate || !endDate) {
+        throw new ApiError("Start date and end date are required");
+      }
+
       const blockSchedules = await this.listBlockScheduleUseCase.execute({
-        ...data,
+        endDate: endDate as string,
+        startDate: startDate as string,
         userId,
       });
 
