0e1dece168a35f7f3c7714896ee59baa3ae66943
Guilherme Piangers <eduardomartinspiangers@gmail.com>
2025-09-01 22:26:00 -0300

    feat: Criando EditBlockScheduleController

---
+++ src/core/scheduling/controllers/editBlockScheduleController/editBlockScheduleController.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from "express";
+import {
+  EditBlockScheduleDTO,
+  EditBlockScheduleUseCase,
+} from "../../useCases/blockScheduling/editBlockScheduleUseCase";
+import { responseError } from "../../../../utils/ResponseError";
+
+export class EditBlockScheduleController {
+  constructor(private editBlockScheduleUseCase: EditBlockScheduleUseCase) {}
+
+  async handle(request: Request, response: Response) {
+    try {
+      const { id } = request.params;
+      const userId = request.user.id;
+      const data = request.body as EditBlockScheduleDTO;
+
+      await this.editBlockScheduleUseCase.execute({ ...data, id }, userId!);
+
+      return response.status(204).send();
+    } catch (err: any) {
+      return responseError(response, err);
+    }
+  }
+}
