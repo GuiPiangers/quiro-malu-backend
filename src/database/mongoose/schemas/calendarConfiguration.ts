b8b3a0dfdd84581e641c425c45e9f096ce283171
Guilherme Piangers <eduardomartinspiangers@gmail.com>
2025-09-08 22:10:03 -0300

    feat: Criando o schema do Mongoose CalendarConfigurationModel

---
+++ src/database/mongoose/schemas/calendarConfiguration.ts
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const workScheduleSchema = new mongoose.Schema(
+  {
+    start: { type: String, required: true },
+    end: { type: String, required: true },
+  },
+  { _id: false },
+);
+
+const dayConfigurationSchema = new mongoose.Schema(
+  {
+    workTimeIncrementInMinutes: { type: Number, required: true },
+    workSchedules: [workScheduleSchema],
+  },
+  { _id: false },
+);
+
+const calendarConfigurationSchema = new mongoose.Schema({
+  userId: { type: String, required: true, index: true },
+  0: dayConfigurationSchema, // Monday
+  1: dayConfigurationSchema, // Tuesday
+  2: dayConfigurationSchema, // Wednesday
+  3: dayConfigurationSchema, // Thursday
+  4: dayConfigurationSchema, // Friday
+  5: dayConfigurationSchema, // Saturday
+  6: dayConfigurationSchema, // Sunday
+});
+
+export const CalendarConfigurationModel = mongoose.model(
+  "CalendarConfiguration",
+  calendarConfigurationSchema,
+);
