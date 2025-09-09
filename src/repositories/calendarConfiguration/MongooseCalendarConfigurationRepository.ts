88b6d8edf150f22ae3780494cf4d11cd89e96b79
Guilherme Piangers <eduardomartinspiangers@gmail.com>
2025-09-09 18:43:49 -0300

    fix: Corrigindo o metodo update do MongooseCalendarConfigurationRepository para utilizar os dias da semana ao invés de index

--- src/repositories/calendarConfiguration/MongooseCalendarConfigurationRepository.ts
+++ src/repositories/calendarConfiguration/MongooseCalendarConfigurationRepository.ts
@@ -21,17 +21,15 @@ export class MongooseCalendarConfigurationRepository
       return null;
     }
 
-    const doc = configDoc as any;
-
     return new CalendarConfiguration({
-      userId: doc.userId,
-      0: doc[0],
-      1: doc[1],
-      2: doc[2],
-      3: doc[3],
-      4: doc[4],
-      5: doc[5],
-      6: doc[6],
+      userId: configDoc.userId,
+      domingo: configDoc.domingo || undefined,
+      segunda: configDoc.segunda || undefined,
+      terca: configDoc.terca || undefined,
+      quarta: configDoc.quarta || undefined,
+      quinta: configDoc.quinta || undefined,
+      sexta: configDoc.sexta || undefined,
+      sabado: configDoc.sabado || undefined,
     });
   }
 
