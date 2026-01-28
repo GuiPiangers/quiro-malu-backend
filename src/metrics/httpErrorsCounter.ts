6c4429a8265b73f6c1e393e2285129f454795c9c
Guilherme Piangers <eduardomartinspiangers@gmail.com>
2026-01-27 22:26:47 -0300

    fix: Prettier

--- src/metrics/httpErrorsCounter.ts
+++ src/metrics/httpErrorsCounter.ts
@@ -4,30 +4,30 @@ import client from "prom-client";
 import { normalizeRoute } from "./utils/normalizeRoute";
 
 const httpErrorsCounter = new client.Counter({
-  name: "http_errors_total",
-  help: "Total de erros HTTP",
-  labelNames: ["method", "route", "status_code", "error_type"],
+    name: "http_errors_total",
+    help: "Total de erros HTTP",
+    labelNames: ["method", "route", "status_code", "error_type"],
 });
 
 register.registerMetric(httpErrorsCounter);
 
 export const httpErrorsCounterMiddleware = (
-  req: Request,
-  res: Response,
-  next: NextFunction
+    req: Request,
+    res: Response,
+    next: NextFunction
 ) => {
-  res.on("finish", () => {
-    if (res.statusCode >= 400) {
-      const route = normalizeRoute(req);
-      const errorType = res.statusCode >= 500 ? "server_error" : "client_error";
+    res.on("finish", () => {
+        if (res.statusCode >= 400) {
+            const route = normalizeRoute(req);
+            const errorType = res.statusCode >= 500 ? "server_error" : "client_error";
 
-      httpErrorsCounter.inc({
-        method: req.method,
-        route,
-        status_code: res.statusCode,
-        error_type: errorType,
-      });
-    }
-  });
-  next();
+            httpErrorsCounter.inc({
+                method: req.method,
+                route,
+                status_code: res.statusCode,
+                error_type: errorType,
+            });
+        }
+    });
+    next();
 };
