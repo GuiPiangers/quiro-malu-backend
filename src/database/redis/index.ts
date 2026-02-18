d64f94b262abc41d94fc8277f936e2a6b8e45827
Guilherme Piangers <eduardomartinspiangers@gmail.com>
2026-02-18 20:49:19 -0300

    fix: Removing external ports of services in docker-compose

--- src/database/redis/index.ts
+++ src/database/redis/index.ts
@@ -1,8 +1,11 @@
 import { Redis } from "ioredis";
 const host = process.env.DB_REDIS_HOST;
 
+const port = Number(process.env.DB_REDIS_PORT) || 6379;
+
 const redis = new Redis({
   host,
+  port,
   maxRetriesPerRequest: null,
 });
 
