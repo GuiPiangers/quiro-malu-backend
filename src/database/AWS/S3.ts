1a90c10ecc049a2fc90139f017bcd9d5cfdcde0c
Guilherme Piangers <eduardomartinspiangers@gmail.com>
2026-03-23 09:05:11 -0300

    fix: Atualizando o acesso a bucket AWS para utilizar acesso externo

--- src/database/AWS/S3.ts
+++ src/database/AWS/S3.ts
@@ -1,18 +1,12 @@
 import { S3Client } from "@aws-sdk/client-s3";
 
-const isDevelopment = process.env.NODE_ENV === "development";
-
 const accessKeyId = process.env.AWS_ACCESS_KEY_ID!;
 const secretAccessKey = process.env.AWS_ACCESS_KEY_SECRET!;
 
-export const s3Client = isDevelopment
-  ? new S3Client({
-      credentials: {
-        accessKeyId,
-        secretAccessKey,
-      },
-      region: process.env.BUCKET_REGION || "sa-east-1",
-    })
-  : new S3Client({
-      region: process.env.BUCKET_REGION || "sa-east-1",
-    });
+export const s3Client = new S3Client({
+  credentials: {
+    accessKeyId,
+    secretAccessKey,
+  },
+  region: process.env.BUCKET_REGION || "sa-east-1",
+});
