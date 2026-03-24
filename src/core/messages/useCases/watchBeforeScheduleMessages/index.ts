0cbe79d135b50bdb01c72a10fd85d61efb7e5592
Guilherme Piangers <eduardomartinspiangers@gmail.com>
2026-03-24 18:46:02 -0300

    chore(messages): add watchBeforeScheduleMessages use case index

---
+++ src/core/messages/useCases/watchBeforeScheduleMessages/index.ts
@@ -0,0 +1,12 @@
+import { BeforeScheduleMessageRepository } from "../../../../repositories/messages/BeforeScheduleMessageRepository";
+import { appEventListener } from "../../../shared/observers/EventListener";
+import { WatchBeforeScheduleMessagesUseCase } from "./watchBeforeScheduleMessagesUseCase";
+
+const beforeScheduleMessageRepository = new BeforeScheduleMessageRepository();
+
+const watchBeforeScheduleMessagesUseCase = new WatchBeforeScheduleMessagesUseCase(
+  beforeScheduleMessageRepository,
+  appEventListener,
+);
+
+export { watchBeforeScheduleMessagesUseCase };
