4326b6a25f3ea3c1da938b3d273fbfd00b81ed07
Guilherme Piangers <eduardomartinspiangers@gmail.com>
2026-03-24 18:45:02 -0300

    feat(repos): add listAll/getById to beforeScheduleMessage

--- src/repositories/messages/BeforeScheduleMessageRepository.ts
+++ src/repositories/messages/BeforeScheduleMessageRepository.ts
@@ -2,6 +2,8 @@ import { Knex } from "../../database/knex";
 import { ETableNames } from "../../database/ETableNames";
 import { ApiError } from "../../utils/ApiError";
 import {
+  BeforeScheduleMessageConfigDTO,
+  GetBeforeScheduleMessageByIdProps,
   IBeforeScheduleMessageRepository,
   SaveBeforeScheduleMessageProps,
 } from "./IBeforeScheduleMessageRepository";
@@ -12,6 +14,48 @@ export class BeforeScheduleMessageRepository
   async save(data: SaveBeforeScheduleMessageProps): Promise<void> {
     try {
       await Knex(ETableNames.BEFORE_SCHEDULE_MESSAGES).insert(data);
+    } catch (error: any) {
+      throw new ApiError(error.message, 500);
+    }
+  }
+
+  async listAll(): Promise<BeforeScheduleMessageConfigDTO[]> {
+    try {
+      const rows = await Knex(ETableNames.BEFORE_SCHEDULE_MESSAGES).select(
+        "id",
+        "userId",
+        "minutesBeforeSchedule",
+        "textTemplate",
+        "isActive",
+      );
+
+      return rows as BeforeScheduleMessageConfigDTO[];
+    } catch (error: any) {
+      throw new ApiError(error.message, 500);
+    }
+  }
+
+  async getById(
+    data: GetBeforeScheduleMessageByIdProps,
+  ): Promise<BeforeScheduleMessageConfigDTO | null> {
+    try {
+      const row = await Knex(ETableNames.BEFORE_SCHEDULE_MESSAGES)
+        .select(
+          "id",
+          "userId",
+          "minutesBeforeSchedule",
+          "textTemplate",
+          "isActive",
+        )
+        .first()
+        .where({
+          id: data.id,
+          userId: data.userId,
+        });
+
+      if (!row) return null;
+
+      return row as BeforeScheduleMessageConfigDTO;
     } catch (error: any) {
       throw new ApiError(error.message, 500);
     }
