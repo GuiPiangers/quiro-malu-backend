2ba40b52db6ad18b777b1eeba1f36d15623f2d1f
Guilherme Piangers <eduardomartinspiangers@gmail.com>
2026-03-24 18:44:56 -0300

    feat(repos): extend beforeScheduleMessage repository interface

--- src/repositories/messages/IBeforeScheduleMessageRepository.ts
+++ src/repositories/messages/IBeforeScheduleMessageRepository.ts
@@ -6,6 +6,25 @@ export type SaveBeforeScheduleMessageProps = {
   isActive: boolean;
 };
 
+export type BeforeScheduleMessageConfigDTO = {
+  id: string;
+  userId: string;
+  minutesBeforeSchedule: number;
+  textTemplate: string;
+  isActive: boolean;
+};
+
+export type GetBeforeScheduleMessageByIdProps = {
+  id: string;
+  userId: string;
+};
+
 export interface IBeforeScheduleMessageRepository {
   save(data: SaveBeforeScheduleMessageProps): Promise<void>;
+
+  listAll(): Promise<BeforeScheduleMessageConfigDTO[]>;
+
+  getById(
+    data: GetBeforeScheduleMessageByIdProps,
+  ): Promise<BeforeScheduleMessageConfigDTO | null>;
 }
