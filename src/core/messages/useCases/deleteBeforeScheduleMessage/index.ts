60a16e7603ebabbc031909a1f2afd1fc1dc2649f
Guilherme Piangers <eduardomartinspiangers@gmail.com>
2026-04-06 17:53:16 -0300

    feat: Create instance for DeleteBeforeScheduleMessageUseCase with necessary dependencies

---
+++ src/core/messages/useCases/deleteBeforeScheduleMessage/index.ts
@@ -0,0 +1,17 @@
+import { beforeScheduleQueue } from "../../../../queues/beforeScheduleMessage";
+import { BeforeScheduleMessageRepository } from "../../../../repositories/messages/BeforeScheduleMessageRepository";
+import { KnexSchedulingRepository } from "../../../../repositories/scheduling/KnexSchedulingRepository";
+import { appEventListener } from "../../../shared/observers/EventListener";
+import { DeleteBeforeScheduleMessageUseCase } from "./DeleteBeforeScheduleMessageUseCase";
+
+const beforeScheduleMessageRepository = new BeforeScheduleMessageRepository();
+const schedulingRepository = new KnexSchedulingRepository();
+
+const deleteBeforeScheduleMessageUseCase = new DeleteBeforeScheduleMessageUseCase(
+  beforeScheduleMessageRepository,
+  schedulingRepository,
+  beforeScheduleQueue,
+  appEventListener,
+);
+
+export { deleteBeforeScheduleMessageUseCase };
