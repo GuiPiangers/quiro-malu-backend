cb3fd3828ccc5dcddb748cdb344d3813800ff6e8
Guilherme Piangers <eduardomartinspiangers@gmail.com>
2026-04-14 14:41:08 -0300

    feat: Add ListWhatsAppMessageLogsByPatientController for handling requests to list WhatsApp message logs by patient

---
+++ src/core/messages/controllers/listWhatsAppMessageLogsByPatientController/ListWhatsAppMessageLogsByPatientController.ts
@@ -0,0 +1,28 @@
+import { Request, Response } from "express";
+import { responseError } from "../../../../utils/ResponseError";
+import { ListWhatsAppMessageLogsByPatientUseCase } from "../../useCases/listWhatsAppMessageLogsByPatient/ListWhatsAppMessageLogsByPatientUseCase";
+
+export class ListWhatsAppMessageLogsByPatientController {
+  constructor(
+    private listWhatsAppMessageLogsByPatientUseCase: ListWhatsAppMessageLogsByPatientUseCase,
+  ) {}
+
+  async handle(request: Request, response: Response) {
+    try {
+      const userId = request.user.id!;
+      const { patientId } = request.params;
+      const { page, limit } = request.query;
+
+      const res = await this.listWhatsAppMessageLogsByPatientUseCase.execute({
+        userId,
+        patientId,
+        page: page != null ? Number(page) : undefined,
+        limit: limit != null ? Number(limit) : undefined,
+      });
+
+      return response.status(200).json(res);
+    } catch (err: any) {
+      return responseError(response, err);
+    }
+  }
+}
