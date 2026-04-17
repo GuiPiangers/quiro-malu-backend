af7830f9b56934254e773f849f65e1f470f45d84
Guilherme Piangers <eduardomartinspiangers@gmail.com>
2026-04-17 11:55:27 -0300

    feat: create sendBirthdayMessage module for handling birthday message sending
    - Added a new module for sending birthday messages via WhatsApp, integrating necessary repositories and providers.
    - Initialized SendBirthdayMessageUseCase with dependencies for message handling and logging.
    - Set up event listener for message events.

---
+++ src/core/messages/useCases/birthdayMessage/sendBirthdayMessage/index.ts
@@ -0,0 +1,28 @@
+import { EvolutionWhatsAppProvider } from "../../../../../providers/whatsapp/EvolutionWhatsAppProvider";
+import { BirthdayMessageRepository } from "../../../../../repositories/messages/BirthdayMessageRepository";
+import { KnexPatientRepository } from "../../../../../repositories/patient/KnexPatientRepository";
+import { KnexWhatsAppInstanceRepository } from "../../../../../repositories/whatsapp/KnexWhatsAppInstanceRepository";
+import { KnexWhatsAppMessageLogRepository } from "../../../../../repositories/whatsapp/KnexWhatsAppMessageLogRepository";
+import { appEventListener } from "../../../../shared/observers/EventListener";
+import { SendBirthdayMessageUseCase } from "./sendBirthdayMessageUseCase";
+
+const birthdayMessageRepository = new BirthdayMessageRepository();
+const patientRepository = new KnexPatientRepository();
+const whatsAppInstanceRepository = new KnexWhatsAppInstanceRepository();
+const whatsAppMessageLogRepository = new KnexWhatsAppMessageLogRepository();
+
+const whatsAppProvider = new EvolutionWhatsAppProvider(
+  process.env.EVOLUTION_API_BASE_URL ?? "",
+  process.env.AUTHENTICATION_API_KEY ?? "",
+);
+
+const sendBirthdayMessageUseCase = new SendBirthdayMessageUseCase(
+  birthdayMessageRepository,
+  patientRepository,
+  whatsAppProvider,
+  whatsAppInstanceRepository,
+  whatsAppMessageLogRepository,
+  appEventListener,
+);
+
+export { sendBirthdayMessageUseCase };
